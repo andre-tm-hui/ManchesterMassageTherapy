87a5963c3d7548adf84e063b0dc267f8fcc733e3
Andre Hui <andre.tm.hui@gmail.com>
2023-11-20 15:37:36 +0000

    fix: created separate layout to support exit anims

---
+++ manchester_massage_therapy/app/layout.tsx
@@ -0,0 +1,27 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { inter } from './fonts';
+import Navbar from './_components/navigation/navbar';
+import Footer from './_components/navigation/footer';
+import { AnimatePresence } from 'framer-motion';
+
+export const metadata: Metadata = {
+  title: 'Manchester Massage Therapy',
+  description: 'The online home of Manchester Massage Therapy',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <body className={`${inter.className} bg-primary`}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
