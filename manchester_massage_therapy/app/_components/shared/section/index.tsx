362fe89028b0c3b7eeaf97cfef45c5600e11562f
Andre Hui <andre.tm.hui@gmail.com>
2023-11-20 15:40:14 +0000

    feat: added fade-in animations

--- manchester_massage_therapy/app/_components/shared/section/index.tsx
+++ manchester_massage_therapy/app/_components/shared/section/index.tsx
@@ -1,12 +1,28 @@
+'use client';
+
 import { ComponentProps } from 'react';
+import { motion } from 'framer-motion';
+
+interface SectionProps extends ComponentProps<'div'> {
+  bgColor?: undefined | string;
+}
 
 export default function Section({
+  bgColor,
   className,
   children,
-}: ComponentProps<'div'>) {
+}: SectionProps) {
   return (
-    <div className={`w-full gap-8 py-16 md:py-32 ${className ?? ''}`}>
-      {children}
+    <div className={`w-full ${bgColor ?? ''}`}>
+      <motion.div
+        className={`w-full gap-8 py-16 md:py-32 ${className ?? ''}`}
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.6, ease: 'easeInOut', delay: 0.3 }}
+      >
+        {children}
+      </motion.div>
     </div>
   );
 }
