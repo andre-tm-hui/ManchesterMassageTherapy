586763085ef16f250d5ad10d4d518335e2cb2908
Andre Hui <andre.tm.hui@gmail.com>
2023-12-06 23:00:15 +0000

    fix: cron

--- manchester_massage_therapy/app/api/cron/fetch/route.ts
+++ manchester_massage_therapy/app/api/cron/fetch/route.ts
@@ -1,6 +1,8 @@
 import { fetchGoogleReview } from "./fetchGoogleReview";
 import { fetchInstagram } from "./fetchInstagram";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
   await fetchInstagram();
   await fetchGoogleReview();
--- manchester_massage_therapy/app/api/cron/refreshInstagramToken.ts
+++ manchester_massage_therapy/app/api/cron/refreshInstagramToken.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
   await axios.get(`https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${process.env.INSTAGRAM_ACCESS_TOKEN}`).catch((_) => {return;});
 }
