c6419a8a7f6add05ba10c911bb10fa1d8e62c2ed
Andre Hui <andre.tm.hui@gmail.com>
2023-12-11 16:16:24 +0000

    fix: nodemailer

--- manchester_massage_therapy/libs/mailing/index.ts
+++ manchester_massage_therapy/libs/mailing/index.ts
@@ -1,7 +1,8 @@
 import nodemailer from 'nodemailer';
 
 let transporter = nodemailer.createTransport({
-    service: 'gmail',
+    port: 465,
+    host: "smtp.gmail.com",
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
