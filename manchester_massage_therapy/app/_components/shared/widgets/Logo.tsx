395465cfaeb9ed80ed4c61bc5480dae74061b2e6
Andre Hui <andre.tm.hui@gmail.com>
2023-12-12 17:15:14 +0000

    style: changed colour

--- manchester_massage_therapy/app/_components/shared/widgets/Logo.tsx
+++ manchester_massage_therapy/app/_components/shared/widgets/Logo.tsx
@@ -18,7 +18,7 @@ export default function Logo({ href, className, size }: LogoProps) {
       )}
     >
       <MmtLogo
-        className='logo'
+        className='text-logo'
         width={size}
         height={size}
         color='var(--logo-rgb)'
