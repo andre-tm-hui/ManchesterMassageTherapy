eb54e49d8458e2e790429162dff8508bb3c07a81
Andre Hui <andre.tm.hui@gmail.com>
2023-12-13 16:28:10 +0000

    fix: email posting tested and working

--- manchester_massage_therapy/app/api/contact/form/route.ts
+++ manchester_massage_therapy/app/api/contact/form/route.ts
@@ -1,42 +1,47 @@
 import nodemailer from 'nodemailer';
 
+export const dynamic = 'force-dynamic'
+
 let transporter = nodemailer.createTransport({
-    port: 465,
-    host: "smtp.gmail.com",
+    port: parseInt(process.env.EMAIL_PORT!),
+    host: process.env.EMAIL_HOST,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
     }})
 
-export async function POST(req: any, res: any) {
-  let mailOptions = [
+export async function POST(req: Request) {
+   const { name, email, phone, subject, message } = await req.json();
+   let mailOptions = [
     {
-      from: process.env.EMAIL_USERNAME,
-      to: req.email,
-      subject: `Thanks for getting in touch, ${req.name}!`,
+      from: "Manchester Massage Therapy <support@manchestermassagetherapy.co.uk>",
+      to: email,
+      subject: `Thanks for getting in touch, ${name}!`,
       text: 
-      `Hi ${req.name}!
+      `Hi ${name}!
       \n\nThanks for getting in touch. We'll get back to you as soon as possible. 
-      \n\nFor your reference, your message was: \n\nSubject: ${req.subject}\n${req.message}
+      \n\nFor your reference, your message was: \n\nSubject: ${subject}\n${message}
       \n\nBest regards, 
       \nThe team at Manchester Massage Therapy
       \n\n\nFeel free to reply to this email if you missed anything in your original message.`
     }, 
     {
-      from: process.env.EMAIL_USERNAME,
+      from: "Manchester Massage Therapy <support@manchestermassagetherapy.co.uk>",
       to: process.env.EMAIL_USERNAME,
-      subject: `New message from ${req.name}: ${req.subject}`,
-      text: `Name: ${req.name} \n Email: ${req.email} \n Phone: ${req.phone ?? 'N/A'} \n Message: ${req.message}`
+      subject: `New message from ${name}: ${subject}`,
+      text: `Name: ${name} \n Email: ${email} \n Phone: ${phone ?? 'N/A'} \n Message: ${message}`
     }
   ]
 
   mailOptions.forEach(mailOption => {
     transporter.sendMail(mailOption, function(error, info){
       if (error) {
-        console.log(error);
+        return Response.json({ message: error, status: 400 });
       } else {
         console.log('Email sent: ' + info.response)
       }
     })    
   });
+
+  return Response.json({ message: "success", status: 200 });
 }
