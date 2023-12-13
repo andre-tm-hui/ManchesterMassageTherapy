0ba901090124ccd90681e192877d90c0c6ae67d9
Andre Hui <andre.tm.hui@gmail.com>
2023-12-13 16:26:59 +0000

    clean

--- manchester_massage_therapy/app/api/cron/refreshInstagramToken.ts
+++ manchester_massage_therapy/app/api/cron/refreshInstagramToken.ts
@@ -3,5 +3,10 @@ import axios from "axios";
 export const dynamic = 'force-dynamic'
 
 export async function GET() {
-  await axios.get(`https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${process.env.INSTAGRAM_ACCESS_TOKEN}`).catch(() => {return;});
+  const res = await axios.get(
+    `https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${process.env.INSTAGRAM_ACCESS_TOKEN}`
+    ).catch(
+      (e) => {return {status: 400, message: e};}
+      ).then((res) =>  {return {status: 400, message: "ok"};});
+  Response.json(res);
 }
