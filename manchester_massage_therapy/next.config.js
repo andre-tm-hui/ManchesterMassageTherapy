1eeb525dbbb7ff06d16c7773e80679430062d741
Andre Hui <andre.tm.hui@gmail.com>
2023-12-22 18:37:40 +0000

    fix: added google content servers

--- manchester_massage_therapy/next.config.js
+++ manchester_massage_therapy/next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: '*.cdninstagram.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.googleusercontent.com',
       }
     ]
   }
